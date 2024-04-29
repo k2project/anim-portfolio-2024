169f03c2cd7224eca2257caff83c601ce2dbe2c0
Kris <kopczkrzy@gmail.com>
2024-04-29 09:55:39 +0100

    Fix to base path for different env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', //// <=== enables static exports
-    basePath: '/anim-portfolio-2024', //<=== path for static assets
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH, //<=== path for static assets
 };
 
 export default nextConfig;
