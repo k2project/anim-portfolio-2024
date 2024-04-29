42494b50fdd825e9106cf0076d040803eadc625d
Kris <kopczkrzy@gmail.com>
2024-04-29 09:22:17 +0100

    Testing failed static images by removing absolute path.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/anim-portfolio-2024', //<=== path for static assets
     output: 'export', //// <=== enables static exports
-    reactStrictMode: true,
+    basePath: '/anim-portfolio-2024', //<=== path for static assets
 };
 
 export default nextConfig;
