67259534f50948edc9cd8bc5d14b6e7506e1316e
Kris <kopczkrzy@gmail.com>
2024-05-02 15:10:24 +0100

    Sections loading improved.
    Because the sections animations depends on window dimensions that are calculated on the first mount, there might be a flickering of the screen due to lighter sections not having the initial styles applied on the first mounting. This fixes the issue.

--- app/components/sections/index.tsx
+++ app/components/sections/index.tsx
@@ -5,6 +5,7 @@ import IntroSection from '@components/sections/intro';
 import useSections from './useSections';
 import AboutSection from './about';
 import TechSection from './tech';
+import { m } from 'framer-motion';
 
 export default function Sections() {
     const { showScrollableSections, enableScrolling } = useSections();
@@ -12,11 +13,17 @@ export default function Sections() {
         <>
             <IntroSection enableScrolling={enableScrolling} />
             {showScrollableSections && (
-                <>
+                <m.div
+                    animate={{ opacity: 1 }}
+                    initial={{ opacity: 0 }}
+                    transition={{
+                        delay: 0.25, // Allows for smooth loading of all sections while the screen dimensions are computed
+                    }}
+                >
                     <DeveloperSection />
                     <AboutSection />
                     <TechSection />
-                </>
+                </m.div>
             )}
         </>
     );
