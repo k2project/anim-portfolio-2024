e6c5aec2d56614e1a581de82996459de3f99a3b3
Kris <kopczkrzy@gmail.com>
2024-05-02 08:17:02 +0100

    Testing assetPrefix in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: process.env.BASE_PATH, //<=== path for static assets
+    assetPrefix: process.env.BASE_PATH, //<=== path for static assets
     images: {
         domains: ['media.licdn.com'],
     },
