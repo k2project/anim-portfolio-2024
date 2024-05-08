9ecdf036c12a7a56fdaa7a936c1907d7b2b08505
Kris <kopczkrzy@gmail.com>
2024-05-08 07:52:38 +0100

    Lazy loading of scrollable sections - performance optimisation

--- app/components/sections/index.tsx
+++ app/components/sections/index.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import DeveloperSection from '@components/sections/developer';
+import { lazy } from 'react';
 import IntroSection from '@components/sections/intro';
 import useSections from './useSections';
-import AboutSection from './about';
-import TechSection from './tech';
 import { m } from 'framer-motion';
-import RecommendationsSection from './recommendations';
-import EndingSection from './ending';
+
+const DeveloperSection = lazy(() => import('./developer'));
+const AboutSection = lazy(() => import('./about'));
+const TechSection = lazy(() => import('./tech'));
+const RecommendationsSection = lazy(() => import('./recommendations'));
+const EndingSection = lazy(() => import('./ending'));
 
 export default function Sections() {
     const { showScrollableSections, enableScrolling } = useSections();
@@ -19,7 +21,7 @@ export default function Sections() {
                     animate={{ opacity: 1 }}
                     initial={{ opacity: 0 }}
                     transition={{
-                        delay: 0.5, // Allows for smooth loading of all sections while the screen dimensions are computed
+                        delay: 0.75, // Allows for smooth loading of all sections while the screen dimensions are computed
                     }}
                 >
                     <DeveloperSection />
