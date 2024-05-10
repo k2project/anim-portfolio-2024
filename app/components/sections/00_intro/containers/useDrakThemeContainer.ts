7a6492895a4fd1f34dcded8fcc8b4645aab2f32c
Kris <kopczkrzy@gmail.com>
2024-05-10 08:36:07 +0100

    Updated useDrakThemeConatiner animation to match previous updates to animations.

--- app/components/sections/00_intro/containers/useDrakThemeContainer.ts
+++ app/components/sections/00_intro/containers/useDrakThemeContainer.ts
@@ -12,8 +12,12 @@ export default function useDarkThemeContainer(): IUseDarkThemeContainer {
     const { scrollY } = useScroll();
     const { windowHeight } = useWindowDimensions();
 
-    const opacity = useTransform(scrollY, [0, 100, windowHeight], [1, 1, 0]);
-    const scale = useTransform(scrollY, [0, 100, windowHeight], [1, 1, 0]);
+    const opacity = useTransform(
+        scrollY,
+        [0, 100, windowHeight * 2],
+        [1, 1, 0]
+    );
+    const scale = useTransform(scrollY, [0, 100, windowHeight * 2], [1, 1, 0]);
 
     return { opacity, scale };
 }
